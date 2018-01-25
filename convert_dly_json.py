3ad3fe38f5c3ce6c07ef36099b44b67f0e0ccb79
csteinlehner <christoph@csteinlehner.com>
2018-01-25 17:23:47 +0200

    Outputs a flat json

--- convert_dly_json.py
+++ convert_dly_json.py
@@ -30,26 +30,29 @@ def readRow(lineOfData):
     rowData["month"] = lineOfData[15:17]
     year = rowData["year"]
     month = rowData["month"]
-    days["stationID"] = rowData["stationID"]
-    days["countryCode"] = rowData["countryCode"]
 
-    yearStr = 'y'+str(year);
-    monthStr ='m'+str(month); 
-    if days.has_key(yearStr)==False:
-        days[yearStr] = {}
-    if days[yearStr].has_key(monthStr)==False:
-        days[yearStr][monthStr] = {}
+    yearStr = str(year)
+    monthStr = str(month)
+    # if days.has_key(yearStr)==False:
+    #     days[yearStr] = {}
+    # if days[yearStr].has_key(monthStr)==False:
+    #     days[yearStr][monthStr] = {}
     element = lineOfData[17:21]
     for x in range(0, 31):
         dayOM = x + 1
         offsetStart = (x*8)+21
         offsetEnd = offsetStart + 8
         dayDat = addMeasurement(element, lineOfData[offsetStart:offsetEnd])
-        day = 'd'+str("%02d" % (dayOM,))
-        if days[yearStr][monthStr].has_key(day)==False:
-            days[yearStr][monthStr][day] = {}
-        days[yearStr][monthStr][day][dayDat[0]] = int(dayDat[1])
-    return rowData
+        dayStr = str("%02d" % (dayOM,))
+        identifier = yearStr+monthStr+dayStr
+        if identifier not in days:
+            days[identifier] = {'year':year,'month':month,'day':dayOM,'countryCode':rowData["countryCode"],'stationID':rowData["stationID"]}
+        days[identifier][dayDat[0]] = int(dayDat[1])
+            # days[yearStr][monthStr][day] = {}
+            # print dayDat[0],
+            # print int(dayDat[1])
+        # days[yearStr][monthStr][day][dayDat[0]] = int(dayDat[1])
+    # return rowData
 
 
 
@@ -59,8 +62,13 @@ with open(csvfile) as fp:
        rowDat = readRow(line)
 
 output = csvfile.split('.')[0]
+# with open(output+'.csv', 'w') as f:
+#     wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+#     for l in days:
+#         print days[l]
+#         # wr.writerow(l.key)
 with open(output+'.json', 'w') as f:
-    # json.dump(days, f, indent=2, sort_keys=True)
-    json.dump(days, f, sort_keys=True)
+    json.dump(days, f, indent=2, sort_keys=True)
+    # json.dump(days, f, sort_keys=True)
     print 'Json written to '+output+'.json'
 
