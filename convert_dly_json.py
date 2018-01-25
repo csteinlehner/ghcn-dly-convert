42099c5c36c479e46539c30e530c36e1d8706d96
csteinlehner <christoph@csteinlehner.com>
2018-01-25 19:14:22 +0200

    Looks like working json for d3

--- convert_dly_json.py
+++ convert_dly_json.py
@@ -20,6 +20,7 @@ csvfile = sys.argv[1]
 days = {}
 days["years"] = []
 
+
 def addMeasurement(measureType, measurement):
         return [measureType.strip(), measurement[0:5].strip(), measurement[5:6].strip(), measurement[6:7].strip(), measurement[7:8].strip()]
 
@@ -35,25 +36,36 @@ def readRow(lineOfData):
     days["countryCode"] = rowData["countryCode"]
 
     yearStr = str(year);
-    monthStr ='m'+str(month);
-    if {'year': yearStr} not in days["years"]:
-        days["years"].append({'year': yearStr, 'months':[]});
-    print days["years"].months;
-    # if days["years"][yearStr]==False:
-    #     days["years"][yearStr] = {}
-        # days[year] = {}
-    # if days[yearStr].has_key(monthStr)==False:
-    #     days[yearStr][monthStr] = {}
-    # element = lineOfData[17:21]
-    # for x in range(0, 31):
-    #     dayOM = x + 1
-    #     offsetStart = (x*8)+21
-    #     offsetEnd = offsetStart + 8
-    #     dayDat = addMeasurement(element, lineOfData[offsetStart:offsetEnd])
-    #     day = 'd'+str("%02d" % (dayOM,))
-    #     if days[yearStr][monthStr].has_key(day)==False:
-    #         days[yearStr][monthStr][day] = {}
-    #     days[yearStr][monthStr][day][dayDat[0]] = int(dayDat[1])
+    monthStr = str(month);
+    currentYearPos = -1
+    currentMonthPos = -1
+    # Check if this year already in days, if not add it with empty month array
+    if not any(d["year"] == yearStr for d in days['years']):
+        days["years"].append({'year': yearStr, 'months':[]})
+        currentYearPos = len(days["years"])-1
+    # Check if this month already in currentYear, if not add it with empty days array  
+    if not any(d['month'] == monthStr for d in days['years'][currentYearPos]['months']):
+        days['years'][currentYearPos]['months'].append({'month': monthStr, 'days':[]})
+        currentMonthPos = len(days['years'][currentYearPos]['months'])-1
+    element = lineOfData[17:21]
+    for x in range(0, 31):
+        dayOM = x + 1
+        offsetStart = (x*8)+21
+        offsetEnd = offsetStart + 8
+        dayDat = addMeasurement(element, lineOfData[offsetStart:offsetEnd])
+        dayStr = str("%02d" % (dayOM,))
+        # Check if this day already in currentMonth, if not add day and empty data 
+        if not any(d['day'] == dayStr for d in days['years'][currentYearPos]['months'][currentMonthPos]['days']):
+            days['years'][currentYearPos]['months'][currentMonthPos]['days'].append({'day': dayStr, 'data':[]})
+
+        for d in days['years'][currentYearPos]['months'][currentMonthPos]['days']:
+            if (d['day'] == dayStr ):
+                d['data'].append({dayDat[0] : int(dayDat[1])})
+       
+        # dayPos = days['years'][currentYearPos]['months'][currentMonthPos]['days'].index(dayOM)
+        # print days['years'][currentYearPos]['months'][currentMonthPos]['days']
+        # days['years'][currentYearPos]['months'][currentMonthPos]['days'][dayStr]
+        # days[yearStr][monthStr][day][dayDat[0]] = int(dayDat[1])
     return rowData
 
 
@@ -61,7 +73,7 @@ def readRow(lineOfData):
 
 with open(csvfile) as fp:
     for cnt, line in enumerate(fp):
-       rowDat = readRow(line)
+       readRow(line)
 
 output = csvfile.split('.')[0]
 with open(output+'.json', 'w') as f:
